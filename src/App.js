a7bdc19993a59af314c0d5a16d892ababd284621
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-05-06 12:58:49 -0700

    used TextFiled and makestyle function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css'
 
-class App extends Component {
-  render() {
+function App() {
+   
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log("submit")
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(2),
+        width: '25ch',
+      },
+    },
+  }));
+
+  const classes = useStyles();
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>
-            Edit
-            <code>
-              src/App.js
-            </code>
-             and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form  className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          required
+          id="outlined-required-name"
+          label="Name"
+          variant="outlined"
+        />
+          <TextField
+          required
+          id="outlined-required-email"
+          label="Email"
+          type="email"
+          variant="outlined"
+        />
+       
+        <TextField
+          required
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+        />
+           {/*  <Button variant="raised" type="submit">Submit</Button> */}
+            <Button type="submit">Submit</Button>
+        </form>
+
+        {/* <div class="form-control">
+        <label for="my-input">Email address</label>
+        <input id="my-input" aria-describedby="my-helper-text" />
+        <span id="my-helper-text">We'll never share your email.</span>
+        </div> */}
+
       </div>
     )
   }
-}
 
 export default App
